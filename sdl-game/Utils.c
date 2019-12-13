885ae50ee7daf5ce56781f26ca325e03d61f8394
frekons <frekons@gmail.com>
2019-12-13 19:27:23 +0300

    .

--- sdl-game/Utils.c
+++ sdl-game/Utils.c
@@ -8,18 +8,17 @@ float magnitude(Point point)
 
 Point vec2_lerp(Point point1, Point point2, float time) // time is between 0, 1
 {
-	Point to_return;
-
 	Point diff = { point2.x - point1.x, point2.y - point1.y };
 
 	float magn = magnitude(diff);
 
-	if (magn <= 0.25f)
+	if (magn <= 0.1f)
 		return point2;
+
+	Point to_return;
 
 	to_return.x = point1.x * (1 - time) + point2.x * time;;
 	to_return.y = point1.y * (1 - time) + point2.y * time;;
-
 
 	return to_return;
 }
